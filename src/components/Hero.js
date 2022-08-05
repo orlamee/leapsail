62c68139bf5e2a83003fb9a5fe9c0af9ec0d2429
Olamilekan Akindele <olalekan@astract.com.ng>
2022-08-05 17:56:10 +0100

    Fixes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,7 @@
 import React from "react";
-// import bg from "../img/bg.png";
+import { Link } from "react-router-dom";
+import google from "../img/google.png";
+import apple from "../img/apple.png";
 
 function Hero() {
   return (
@@ -10,6 +12,10 @@ function Hero() {
             <h2 className="mb-5">We are revolutionising<br/>your concept of comfort.</h2>
             <h6 className="mb-5">Created with a can do and achievers mindset. A self-onboarding <br/>application with features that open you to a new way.</h6>
             <a className='btn btn-warning text-white px-4 py-3 rounded-pill' href="/">Get Started</a>
+            <div className="mt-5">
+              <Link to="/"><img src={apple} alt="" title="Download on Appstore" className="img-fluid me-3" /></Link>
+              <Link to="/"><img src={google} alt="" title="Download on Playstore" className="img-fluid me-3" /></Link>
+            </div>
           </div>
           <div className="col-md-6 left"></div>
         </div>
